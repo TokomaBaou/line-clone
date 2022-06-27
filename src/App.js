ca97a0ee4ed15f183c97a3f69b96e9dc8d7f7edf
Macart <86038701+TokomaBaou@users.noreply.github.com>
2022-06-27 23:01:40 +0900

    SignIn

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./styles.css";
+import { SignIn } from "./components/SignIn";
+import { useAuthState } from "react-firebase-hooks";
+import { auth } from "./firebase.js";
+import { Line } from "./components/Line";
 
-export default function App() {
-  return (
-    <div className="App">
-      <SignIn />
-    </div>
-  );
-}
+export const App = () => {
+  const user = useAuthState(auth);
+  return <div className="App">{user ? <Line /> : <SignIn />}</div>;
+};
